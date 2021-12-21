449141291ff1d74d5bae408c73668c106752891d
Shah Haamid <80157961+Hames-1616@users.noreply.github.com>
2021-12-21 20:28:13 +0530

    Create gross salary.c

---
+++ gross salary.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+
+ int bs, rent, da, s;
+
+ printf("enter the basic salary ");
+ scanf("%d",&bs);
+ rent (20.0)*bs;
+ da=(40.0) bs;
+ s=bs+rent+da;
+
+printf("gross salary = %d",s);
+}
